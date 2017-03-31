0a3071492674b71f94a6e35c3d65c89f06493c96
Damien Garaud <damien.garaud@oslandia.com>
2017-03-31 11:01:48 +0200

    osm-history: add a python script which extracts changesets
    Read a changesets osm file (XML) and writes some features in a CSV file.

---
+++ src/extract-changesets.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+"""Extract changeset from a OSM history changeset file
+"""
+
+import sys
+import re
+import json
+
+
+id_re = r'id="(?P<id>\d+)"'
+created_re = r'created_at="(?P<created>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z)"'
+# closed_re = r'closed_at="(?P<closed>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z)"'
+userid_re = r'.*uid="(?P<uid>\d+)"'
+float_re = r'[-+]?[0-9]*\.?[0-9]*.'
+box_re = r'min_lat="(?P<min_lat>RE)" min_lon="(?P<min_lon>RE)" max_lat="(?P<max_lat>RE)" max_lon="(?P<max_lon>RE)"'.replace('RE', float_re)
+num_re = r'num_changes="(\d+)"'
+comments_re = r'comments_count="(\d+)"'
+changeset_pattern = re.compile(r' '.join([r'\s+<changeset',
+                                          id_re,
+                                          created_re,
+                                          # closed_re,
+                                          userid_re,
+                                          box_re,
+                                          num_re,
+                                          comments_re]))
+tag_re = re.compile(r'  <tag k="(?P<key>.*)" v="(?P<value>.*)"/>')
+
+
+def main(fname, output_fname):
+    with open(fname) as fobj, open(output_fname, 'w') as out:
+        out.write("id,created,uid,min_lat,min_lon,max_lat,max_lon,num_changes,comments_count,key,value\n")
+        tags = []
+        multiple_lines = []
+        # values = []
+        for xmlline in fobj:
+            match = changeset_pattern.match(xmlline)
+            if match:
+                line = ",".join(match.groups()) + ","
+                # There is not tags for this changeset
+                if xmlline.endswith('/>\n'):
+                    out.write(line + ',\n')
+                    continue
+                multiple_lines = []
+                tags = []
+            else:
+                tagmatch = tag_re.match(xmlline)
+                if tagmatch:
+                     tags.append(tuple(tagmatch.groups()))
+                # if </changeset>, you can write all tags
+                # one line by tag. repeat the first line for each
+                if '</changeset>' in xmlline:
+                    for key, value in tags:
+                        multiple_lines.append(line + key + ',"' + value + '"')
+                    out.write("\n".join(multiple_lines))
+                    out.write("\n")
+
+
+if len(sys.argv) != 3:
+    print("ERROR: need an input file and output file")
+    sys.exit(0)
+
+INPUT_FNAME = sys.argv[1]
+OUTPUT_FNAME = sys.argv[2]
+main(INPUT_FNAME, OUTPUT_FNAME)
