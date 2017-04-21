ed6f616fb8b7276d551132e35cff1256f8485a81
Raphaël Delhome <raphael.delhome@oslandia.com>
2017-04-21 15:19:38 +0200

    Integrate new metadata extraction functions into Luigi tasks

--- src/osm-tasks.py
+++ src/osm-tasks.py
@@ -12,7 +12,7 @@ import numpy as np
 
 import osmparsing
 import tagmetanalyse
-from utils import group_count, group_nunique, group_stats
+import utils
 
 class OSMHistoryParsing(luigi.Task):
 
@@ -122,7 +122,7 @@ class OSMTagMetaAnalysis(luigi.Task):
             fulltaganalys.to_csv(outputflow, date_format='%Y-%m-%d %H:%M:%S')
 
 
-            class OSMElementEnrichment(luigi.Task):
+class OSMElementEnrichment(luigi.Task):
     """ Luigi task: building of new features for OSM element history
     """
     datarep = luigi.Parameter("data")
@@ -218,34 +218,14 @@ class ElementMetadataExtract(luigi.Task):
         return luigi.LocalTarget(self.outputpath())
 
     def requires(self):
-        return OSMHistoryParsing(self.datarep, self.dsname)
+        return OSMElementEnrichment(self.datarep, self.dsname)
 
     def run(self):
         with self.input().open('r') as inputflow:
             osm_elements = pd.read_csv(inputflow,
                                        index_col=0,
                                        parse_dates=['ts'])
-        elem_md = (osm_elements.groupby(['elem', 'id'])['version']
-                   .max()
-                   .reset_index())
-        elem_md = pd.merge(elem_md, osm_elements[['elem','id','version','visible']],
-                           on=['elem','id','version'])
-        elem_md['n_user'] = (osm_elements.groupby(['elem', 'id'])['uid']
-                             .nunique()
-                             .reset_index())['uid']
-        elem_md['n_chgset'] = (osm_elements.groupby(['elem', 'id'])['chgset']
-                               .nunique()
-                               .reset_index())['chgset']
-        elem_md['created_at'] = (osm_elements.groupby(['elem', 'id'])['ts']
-                                 .min()
-                                 .reset_index()['ts'])
-        elem_md['lastmodif_at'] = (osm_elements.groupby(['elem', 'id'])['ts']
-                                   .max()
-                                   .reset_index()['ts'])
-        elem_md['lifecycle'] = elem_md.lastmodif_at - elem_md.created_at
-        timehorizon = pd.to_datetime("2017-02-13 00:00:00")
-        elem_md.loc[elem_md.visible.values, "lifecycle"] = timehorizon - elem_md.loc[elem_md.visible.values, "created_at"]
-        elem_md['mntime_between_modif'] = (elem_md.lifecycle / elem_md.version).astype('timedelta64[m]')
+        elem_md = utils.extract_elem_metadata(osm_elements)
         with self.output().open('w') as outputflow:
             elem_md.to_csv(outputflow, date_format='%Y-%m-%d %H:%M:%S')
 
@@ -271,54 +251,7 @@ class ChangeSetMetadataExtract(luigi.Task):
             osm_elements = pd.read_csv(inputflow,
                                        index_col=0,
                                        parse_dates=['ts'])
-        # Basic features: change set identification, time-related features
-        chgset_md = (osm_elements.groupby(['chgset', 'uid'])['elem']
-                    .count().reset_index())
-        chgset_md['opened_at'] = (osm_elements.groupby('chgset')['ts']
-                                  .min()
-                                  .reset_index()['ts'])
-        chgset_md.sort_values(by=['uid','opened_at'], inplace=True)
-        chgset_md['user_lastchgset_inhour'] = (chgset_md.groupby('uid')['opened_at']
-                                        .diff())
-        chgset_md.user_lastchgset_inhour = (chgset_md.user_lastchgset_inhour
-                                            .astype('timedelta64[h]'))
-        chgset_md['lastmodif_at'] = (osm_elements.groupby('chgset')['ts']
-                                     .max()
-                                     .reset_index()['ts'])
-        chgset_md['duration_insec'] = (chgset_md.lastmodif_at
-                                       - chgset_md.opened_at)
-        chgset_md.duration_insec = (chgset_md.duration_insec
-                                    .astype('timedelta64[s]'))
-
-        # Modification-related features
-        chgset_md = group_count(chgset_md, osm_elements, 'chgset', 'id',
-                                '_modif')
-        osmmodif_cr = osm_elements.query("init")        
-        chgset_md = group_count(chgset_md, osmmodif_cr, 'chgset', 'id',
-                                  '_modif_cr')
-        osmmodif_del = osm_elements.query("not init and not visible")
-        chgset_md = group_count(chgset_md, osmmodif_del, 'chgset', 'id',
-                                  '_modif_del')
-        osmmodif_imp = osm_elements.query("not init and visible")
-        chgset_md = group_count(chgset_md, osmmodif_imp, 'chgset', 'id',
-                                  '_modif_imp')
-
-        # Number of modifications per unique element
-        contrib_byelem = (osm_elements.groupby(['elem', 'id', 'chgset'])['version']
-                          .count()
-                          .reset_index())
-        chgset_md = group_stats(chgset_md, contrib_byelem, 'chgset', 'version',
-                                'n', '_modif_byelem')
-        # Element-related features
-        chgset_md = group_nunique(chgset_md, osm_elements, 'chgset', 'id',
-                                  '')        
-        osmelem_cr = osm_elements.query("init and available")
-        chgset_md = group_nunique(chgset_md, osmelem_cr, 'chgset', 'id', '_cr')
-        osmelem_imp = osm_elements.query("not init and visible and available")
-        chgset_md = group_nunique(chgset_md, osmelem_imp, 'chgset', 'id', '_imp')
-        osmelem_del = osm_elements.query("not init and not visible and not available")
-        chgset_md = group_nunique(chgset_md, osmelem_del, 'chgset', 'id', '_del')
-        
+        chgset_md = utils.extract_chgset_metadata(osm_elements)
         with self.output().open('w') as outputflow:
             chgset_md.to_csv(outputflow, date_format='%Y-%m-%d %H:%M:%S')
 
@@ -345,131 +278,11 @@ class UserMetadataExtract(luigi.Task):
     def run(self):
         with self.input()['chgsets'].open('r') as inputflow:
             chgset_md = pd.read_csv(inputflow, index_col=0)
-        
         with self.input()['enrichhist'].open('r') as inputflow:
             osm_elements = pd.read_csv(inputflow,
                                        index_col=0,
                                        parse_dates=['ts'])
-
-        # Basic features: nb change sets, timestamps
-        user_md = (osm_elements.groupby('uid')['chgset']
-                   .nunique()
-                   .reset_index())
-        user_md.colnames = ['uid', 'n_chgset']
-        user_md['first_at'] = (osm_elements.groupby('uid')['ts']
-                               .min()
-                               .reset_index()['ts'])
-        user_md['last_at'] = (osm_elements.groupby('uid')['ts']
-                              .max()
-                              .reset_index()['ts'])
-        user_md['activity'] = user_md.last_at - user_md.first_at
-
-        # Change set-related features
-        user_md = group_stats(user_md, chgset_md, 'uid',
-                              'user_lastchgset_inhour', 't',
-                              '_between_chgsets_inhour')
-        user_md = group_stats(user_md, chgset_md, 'uid', 'duration_insec',
-                                        'd', '_chgset_insec')
-        user_md = group_stats(user_md, chgset_md, 'uid', 'n_elem_modif',
-                                  'n', '_modif_bychgset')
-        user_md = group_stats(user_md, chgset_md, 'uid', 'n_elem',
-                                  'n', '_elem_bychgset')
-
-        # Update features
-        user_md = group_stats(user_md, osm_elements, 'uid', 'nextmodif_in',
-                                  't', '_update_inhour')
-        osmelem_corr = osm_elements.query("willbe_corr")
-        user_md = group_stats(user_md, osmelem_corr, 'uid', 'nextcorr_in',
-                                  't', '_corr_inhour')
-        user_md = group_count(user_md, osmelem_corr, 'uid', 'willbe_corr',
-                                  '_corr')
-        osmelem_autocorr = osm_elements.query("willbe_autocorr")
-        user_md = group_stats(user_md, osmelem_autocorr, 'uid',
-                                  'nextauto_in', 't', '_autocorr_inhour')
-        user_md = group_count(user_md, osmelem_autocorr, 'uid',
-                                  'willbe_autocorr', '_autocorr')
-        
-        # Modification-related features
-        user_md = group_count(user_md, osm_elements, 'uid', 'id', '_modif')
-        #
-        osmmodif_cr = osm_elements.query("init")        
-        user_md = group_count(user_md, osmmodif_cr, 'uid', 'id',
-                                  '_modif_cr')
-        osmmodif_cr_utd = osmmodif_cr.query("up_to_date")
-        user_md = group_count(user_md, osmmodif_cr_utd, 'uid', 'id',
-                                  '_modif_crutd')
-        osmmodif_cr_mod = osmmodif_cr.query("not up_to_date and available")
-        user_md = group_count(user_md, osmmodif_cr_mod, 'uid', 'id',
-                                  '_modif_crmod')
-        osmmodif_cr_del = osmmodif_cr.query("not up_to_date and not available")
-        user_md = group_count(user_md, osmmodif_cr_del, 'uid', 'id',
-                                  '_modif_crdel')
-        #
-        osmmodif_del = osm_elements.query("not init and not visible")
-        user_md = group_count(user_md, osmmodif_del, 'uid', 'id',
-                                  '_modif_del')
-        osmmodif_del_utd = osmmodif_del.query("not available")
-        user_md = group_count(user_md, osmmodif_del_utd, 'uid', 'id',
-                                  '_modif_delutd')
-        osmmodif_del_rebirth = osmmodif_del.query("available")
-        user_md = group_count(user_md, osmmodif_del_rebirth, 'uid', 'id',
-                                  '_modif_delrebirth')
-        user_md = group_stats(user_md, osmmodif_del, 'uid', 'version',
-                                  'v', '_modif_del')
-        #
-        osmmodif_imp = osm_elements.query("not init and visible")
-        user_md = group_count(user_md, osmmodif_imp, 'uid', 'id',
-                                  '_modif_imp')
-        osmmodif_imp_utd = osmmodif_imp.query("up_to_date")
-        user_md = group_count(user_md, osmmodif_imp_utd, 'uid', 'id',
-                                  '_modif_imputd')
-        osmmodif_imp_mod = osmmodif_imp.query("not up_to_date and available")
-        user_md = group_count(user_md, osmmodif_imp_mod, 'uid', 'id',
-                                  '_modif_impmod')
-        osmmodif_imp_del = osmmodif_imp.query("not up_to_date and not available")
-        user_md = group_count(user_md, osmmodif_imp_del, 'uid', 'id',
-                                  '_modif_impdel')
-        user_md = group_stats(user_md, osmmodif_imp, 'uid', 'version',
-                                  'v', '_modif_imp')
-        
-        # Number of modifications per unique element
-        contrib_byelem = (osm_elements.groupby(['elem', 'id', 'uid'])['version']
-                          .count()
-                          .reset_index())
-        user_md = group_stats(user_md, contrib_byelem, 'uid', 'version',
-                                  'n', '_modif_byelem')
-        user_md = group_count(user_md, contrib_byelem.query("version==1"),
-                                  'uid', 'id', '_with_1_contrib')
-
-        # User-related features
-        user_md = group_nunique(user_md, osm_elements, 'uid', 'id', '')
-        osmelem_cr = osm_elements.query("init and available")
-        user_md = group_nunique(user_md, osmelem_cr, 'uid', 'id', '_cr')
-        user_md = group_stats(user_md, osmelem_cr, 'uid', 'vmax',
-                                  'v', '_cr')
-        osmelem_cr_wrong = osm_elements.query("init and not available")
-        user_md = group_nunique(user_md, osmelem_cr_wrong, 'uid', 'id',
-                                    '_cr_wrong')
-        user_md = group_stats(user_md, osmelem_cr_wrong, 'uid', 'vmax',
-                                  'v', '_cr_wrong')
-        osmelem_imp = osm_elements.query("not init and visible and available")
-        user_md = group_nunique(user_md, osmelem_imp, 'uid', 'id', '_imp')
-        user_md = group_stats(user_md, osmelem_imp, 'uid', 'vmax',
-                                  'v', '_imp')
-        osmelem_imp_wrong = osm_elements.query("not init and visible and not available")
-        user_md = group_nunique(user_md, osmelem_imp_wrong, 'uid', 'id', '_imp_wrong')
-        user_md = group_stats(user_md, osmelem_imp_wrong, 'uid', 'vmax',
-                                  'v', '_imp_wrong')
-        osmelem_del = osm_elements.query("not init and not visible and not available")
-        user_md = group_nunique(user_md, osmelem_del, 'uid', 'id', '_del')
-        user_md = group_stats(user_md, osmelem_del, 'uid', 'vmax',
-                                  'v', '_del')
-        osmelem_del_wrong = osm_elements.query("not init and not visible and available")
-        user_md = group_nunique(user_md, osmelem_del_wrong, 'uid', 'id', '_del_wrong')
-        user_md = group_stats(user_md, osmelem_del_wrong, 'uid', 'vmax',
-                                  'v', '_del_wrong')
-    
-        # Metadata saving
+        user_md = utils.extract_user_metadata(osm_elements, chgset_md)
         with self.output().open('w') as outputflow:
             user_md.to_csv(outputflow, date_format='%Y-%m-%d %H:%M:%S')
 
