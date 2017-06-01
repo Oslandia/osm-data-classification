72c4c74b041a4625d45428e546db2bf07bc8600f
Damien Garaud <damien.garaud@oslandia.com>
2017-06-01 16:41:14 +0200

    Python script to extract and compute the most used OSM editors

---
+++ src/extract-user-editor.py
@@ -0,0 +1,116 @@
+# coding: utf-8
+
+"""Read and process the occurrence of OSM editors used by the users (from a CSV
+file) : uid,value,num
+
+Such as:
+
+- uid: user id
+- value: name of the editor as string
+- num: number of occurrence
+"""
+
+
+
+import re
+import string
+import logging
+
+import pandas as pd
+
+FORMAT = '%(asctime)s :: %(levelname)s :: %(funcName)s : %(message)s'
+logging.basicConfig(format=FORMAT, level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+# multiple cases  (where X, Y are digits, 'l' a letter):
+
+#   - name/X.Yl
+#   - name X.Yl
+#   - name X
+#   - name X.Y.Z
+#   - name other stuff X.Y.Z-l
+
+DISCARD_CHAR = '!?#~,()[]+'
+table = str.maketrans(DISCARD_CHAR, " " * len(DISCARD_CHAR))
+
+
+def editor_name(value):
+    """Try to extract the full name of the editor
+
+    value: str
+        name of used OSM editor extracted from the changeset history
+
+    Return a string
+    """
+    # find the first digit occurrence and take the left-side without space or
+    # '/'
+    pattern = r"([\sa-zA-Z4_\.]+).*"
+    value = value.lower().translate(table)
+    return "".join(re.split(pattern, value)).strip()
+
+
+def main(fname, output_fname):
+    logger.info("read the CSV file '%s'", fname)
+    df = pd.read_csv(fname)
+    logger.info("retrieve the unique editor name")
+    df['fullname'] = df['value'].apply(editor_name)
+    logger.info("group by uid,fullname")
+    # df.groupby(['uid', 'fullname'])['num'].sum()
+    return df
+
+
+# what to extract
+# - number of unique editor used by user (high value => experienced user?)
+
+# two choices
+
+# simple one but count 4 JOSM if JOSM has been used 4 times by a user
+# (df.groupby('fullname')['num']
+#  .count()
+#  .sort_values(ascending=False))
+
+# discard multiple occurrence of an editor name (different versions for instance)
+def editor_count(df):
+    """count the number of time an editor is used
+    """
+    return (df.groupby(['uid', 'fullname'])['num']
+            .count()
+            .reset_index()
+            .groupby('fullname')['uid']
+            .count()
+            .sort_values(ascending=False))
+
+# some_trouble_name = ['Go Map!! 1.1', 'rosemary v0.3.11', 'Level0 v1',
+#                      'ArcGIS Editor for OpenStreetMap (2.1)', 'OsmAnd~ 2.0.0#9942M',
+#                      'QGIS OSM v0.5', 'OsmAnd+ 1.8.3']
+
+# the problem to groupby fullname and sum 'num' is that you can have some rare
+# editor which can carry out a large amount of modifications, i.e. changesets
+
+
+# - top 10 or 20 of used editors
+# - occurrence of used selected editors for each user (by top)
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) != 3:
+        print("ERROR: need an input file and output file")
+        sys.exit(0)
+
+    INPUT_FNAME = sys.argv[1]
+    # INPUT_FNAME = "/home/dag/data/osland-ia/soft-used-by-users.csv"
+    OUTPUT_FNAME = sys.argv[2]
+    # OUTPUT_FNAME = None
+    df = main(INPUT_FNAME, OUTPUT_FNAME)
+    data = (editor_count(df)
+            .to_frame()
+            .reset_index()
+            .rename_axis({"uid": "num"}, axis=1))
+    data['ratio'] = data['num'] / data['num'].sum() * 100.
+    data['cumulative'] = data['ratio'].cumsum()
+    # after an analysis, we want to take the first 15th most used editors. The
+    # other editors represent only less than 5% of the total used editors.
+    selection = data.fullname[:16].tolist() + ['other']
+    logger.info("Write the '%s' data file", OUTPUT_FNAME)
+    data.to_csv(OUTPUT_FNAME, index=False)
