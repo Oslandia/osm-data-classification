1e5d61817d2cec32334db84079ad3469d815eee3
Raphaël Delhome <raphael.delhome@oslandia.com>
2017-08-28 18:19:15 +0200

    Build a new task for copying element metadata in the osm database

--- src/geom_tasks.py
+++ src/geom_tasks.py
@@ -7,18 +7,21 @@ OSM metadata and their geometry
 import luigi
 import luigi.contrib.postgres as lpg
 from luigi.format import MixedUnicodeBytes
+import pandas as pd
 
 import analysis_tasks
 
 class OSMElementTableCreation(lpg.PostgresQuery):
     """ Luigi task:
     """
-    host = luigi.Parameter("localhost")
-    database = luigi.Parameter("osm")
-    user = luigi.Parameter("rde")
-    password = luigi.Parameter("")
-    table = luigi.Parameter("bordeaux_metropole_elements")
-    query = luigi.Parameter("""
+    dsname = luigi.Parameter("bordeaux-metropole")
+
+    host = "localhost"
+    database = "osm"
+    user = "rde"
+    password = ""
+    table = "_".join([dsname.task_value("", "").replace("-", "_"), "elements"])
+    query = """
         DROP TABLE IF EXISTS {0};
         CREATE TABLE {0}(
                elem varchar,
@@ -26,6 +29,7 @@ class OSMElementTableCreation(lpg.PostgresQuery):
                first_at timestamp,
                last_at timestamp,
                lifespan float,
+               n_inscription_days float,
                n_activity_days float,
                version int,
                n_chgset int,
@@ -38,5 +42,32 @@ class OSMElementTableCreation(lpg.PostgresQuery):
                first_ug int,
                last_ug int
         );
-        """.format(table.task_value("", "")))
+        """.format(table)
 
+class OSMElementTableCopy(lpg.CopyToTable):
+    datarep = luigi.Parameter("data")
+    dsname = luigi.Parameter("bordeaux-metropole")
+
+    host = "localhost"
+    database = "osm"
+    user = "rde"
+    password = ""
+    table = "_".join([dsname.task_value("", "").replace("-", "_"), "elements"])
+    columns = [("elem", "varchar"), ("id", "bigint"),
+               ("first_at", "timestamp"), ("last_at", "timestamp"),
+               ("lifespan", "float"), ("n_inscription_days", "float"),
+               ("n_activity_days", "float"), ("version", "int"),
+               ("n_chgset", "int"), ("n_user", "int"), ("n_autocorr", "int"),
+               ("n_corr", "int"), ("visible", "boolean"), ("first_uid", "int"),
+               ("last_uid", "int"), ("first_ug", "int"), ("last_ug", "int")]
+    def requires(self):
+        return analysis_tasks.ElementMetadataExtract(self.datarep, self.dsname)
+
+    def rows(self):
+        """
+        Return/yield tuples or lists corresponding to each row to be inserted.
+        """
+        with self.input().open('r') as fobj:
+            fobj.readline() # Skip the header line
+            for line in fobj:
+                yield line.strip('\n').split(',') # the separator is ','
