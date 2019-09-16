9de32fcf229d542e522301baea12511d82c79dd7
Damien Garaud <damien.garaud@oslandia.com>
2019-09-16 10:43:13 +0200

    update the script about the history changesets processing

--- osmdq/process-changesets-user-history.py
+++ osmdq/process-changesets-user-history.py
@@ -2,14 +2,16 @@
 
 """Count the number changesets by user, i.e. uid
 
-This script used the dask library to handle the large input CSV file, ~210Go,
+This script used the dask library to handle the large input CSV file, +30Gb,
 about the changesets history.
 """
 
 
 import numpy as np
 
+import dask.config
 from dask import dataframe as dd
+
 
 # Column types of the input CSV file
 DTYPE = {'id': np.dtype(int),
@@ -37,6 +39,7 @@ def nb_changeset_by_uid(data):
            .count())
     return grp.compute()
 
+
 def distinct_software_by_uid(data):
     """retrieve the software used to edited OSM by user
 
@@ -52,22 +55,28 @@ if __name__ == '__main__':
     import os
     import argparse
 
+    dask.config.set(num_workers=8)
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('analyze', help='type of the analyze: changeset or editor', type=str)
+    parser.add_argument('analyze', help='type of the analyze: changeset or editor', type=str, choices=("changeset", "editor"))
+    parser.add_argument('-i', '--input', required=True, help='name of the input csv file (changesets history)')
     parser.add_argument('-o', '--output', help='name of the output file')
     args = parser.parse_args()
 
     analyze = args.analyze
-    if analyze not in ['changeset', 'editor']:
-        raise ValueError("wrong analyze name: 'changeset' or 'editor'")
+    # if analyze not in ['changeset', 'editor']:
+        # raise ValueError("wrong analyze name: 'changeset' or 'editor'")
 
     if args.output is None:
         outpath = os.path.join('./data', 'output-extracts', 'all-' + analyze  + 's-by-user.csv')
-    print(outpath)
 
-    fname = "data/output-extracts/changesets-full-17-03-22.csv"
-    print("dask read the CSV '{}'".format(fname))
-    data = dd.read_csv(fname, blocksize=2**32, dtype=DTYPE)
+    if not os.path.isfile(args.input):
+        print("The file '{}' does not exist.".format(args.input))
+        parser.exit(0)
+
+    print("dask read the CSV '{}'".format(args.input))
+    blocksize = 500e6  # 500MB chunks
+    data = dd.read_csv(args.input, blocksize=blocksize, dtype=DTYPE)
 
     print("data processing")
     if analyze == 'changeset':
@@ -75,4 +84,5 @@ if __name__ == '__main__':
     if analyze == 'editor':
         result = distinct_software_by_uid(data)
     print("writing results")
-    result.to_csv(outpath)
+    result.to_csv(args.output)
+    print("output file '{}'".format(args.output))
